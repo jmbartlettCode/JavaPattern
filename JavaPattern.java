c7f5cc8567182be3843aa40ff7e0be56c2b8976e
jmbartlettCode <73142613+jmbartlettCode@users.noreply.github.com>
2020-11-10 12:01:18 -0600

    Add files via upload

---
+++ JavaPattern.java
@@ -0,0 +1,14 @@
+/*
+ * Josh Bartlett
+ * Assignment 1.3
+ * The purpose of this program is to simply print out the word JAVA in a pattern.
+ */
+
+public class JavaPattern {
+    public static void main(String[] args) {
+        System.out.println("    J     A     V     V     A");
+        System.out.println("    J    A A     V   V     A A");
+        System.out.println("J   J   AAAAA     V V     AAAAA");
+        System.out.println(" J J   A     A     V     A     A");
+    }
+}
